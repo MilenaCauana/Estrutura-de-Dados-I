8e6bc942874f883e5b8a2abd62ca431839438c39
unknown <milenarejani@gmail.com>
2025-05-26 17:50:14 -0300

    Arquivo.h feito e criei o .c

---
+++ TAD - Tipo Abstrato de Dados/Implementação/float_vector.c
@@ -0,0 +1,17 @@
+/*Implementação TAD - Milena Cauana
+
+    ARQUIVO DE IMPLEMENTAÇÃO!
+
+    Exercicio:
+    Crie um TAD de um vetor de float:
+        * O vetor tem uma capacidade máxima (número máximo de elementos);
+        * O vetor informa seu tamanho(quantidade de elementos armazenados atualmente);
+
+    Funções:
+        * size(tad vector); //Retorna o tamanho do vetor (numero atual de elementos inderidos)
+        * capacity(tad vector); //Retorna a capacidade do vetor (numero máximo de elementos)
+        * at(tad vector, int index); //Retorna o elemento do índice "index" com bound-checked
+        * get(tad vector, int index); //Retorna o elemento do index
+        * append(tad vector, float float val); //Adiciona o valor 'bal' no final do vetor. Lança um erro se estiver cheio.
+        * set(tad vector, int index, float val); //Atribui o valor 'val' no indice 'index' do vetor de tipo 'tad'. Lança um erro se o índice for inválido
+        * print (tad vector); //Imprime todos os elementos do vetor. */
---
+++ TAD - Tipo Abstrato de Dados/Implementação/float_vector.h
@@ -0,0 +1,35 @@
+/*Implementação TAD - Milena Cauana
+
+    ARQUIVO DE ESPECIFICAÇÃO!
+
+    Exercicio:
+    Crie um TAD de um vetor de float:
+        * O vetor tem uma capacidade máxima (número máximo de elementos);
+        * O vetor informa seu tamanho(quantidade de elementos armazenados atualmente);
+
+    Funções:
+        * size(tad vector); //Retorna o tamanho do vetor (numero atual de elementos inderidos)
+        * capacity(tad vector); //Retorna a capacidade do vetor (numero máximo de elementos)
+        * at(tad vector, int index); //Retorna o elemento do índice "index" com bound-checked
+        * get(tad vector, int index); //Retorna o elemento do index
+        * append(tad vector, float float val); //Adiciona o valor 'bal' no final do vetor. Lança um erro se estiver cheio.
+        * set(tad vector, int index, float val); //Atribui o valor 'val' no indice 'index' do vetor de tipo 'tad'. Lança um erro se o índice for inválido
+        * print (tad vector); //Imprime todos os elementos do vetor. */
+
+//Primeiro iremos definir um tipo de dado, no geral, se esconde a implementação da estruturaq de dados (da struct)
+typedef struct float_vetor floatVector; //Aqui ele diz: "Existe uma estrutura chamada float_vector, que está em algum lugar, e eu estou dando um novo nome pra essa estrutura -> floatVector
+
+//Agora, temos de colocar as definições das funções
+
+    //Essas duas funções que virão, servem para criação e desalocação
+    floatVector = create(int capacity);
+    void destroy(floatVector **vec);
+
+    //Funções já definidas pelo exercício
+    int size(const floatVector *vec);
+    capacity(const floatVector *vec);
+    at(const floatVector *vec, int index);
+    get(const floatVector *vec, int index);
+    append(const floatVector *vec, float val);
+    set(const floatVector *vec, int index, float val);
+    print (const floatVector *vec);
