057bf494ae5d51d04e6278353ee889cf698c4f7b
Milena Cauana <milenarejani@gmail.com>
2025-05-27 17:52:11 -0300

    Melhorando

--- TAD - Tipo Abstrato de Dados/FloatVectorTADImplementation/float_vector2.h
+++ TAD - Tipo Abstrato de Dados/FloatVectorTADImplementation/float_vector2.h
@@ -16,6 +16,8 @@
         * set(tad vector, int index, float val); //Atribui o valor 'val' no indice 'index' do vetor de tipo 'tad'. Lança um erro se o índice for inválido
         * print (tad vector); //Imprime todos os elementos do vetor. */
 
+//#include<stdio.h> //Ele adicionará no Main essa biblioteca tbm, mas não é uma boa prática de programa
+
 //Primeiro iremos definir um tipo de dado, no geral, se esconde a implementação da estruturaq de dados (da struct)
 
 typedef struct float_vector floatVector; //Aqui ele diz: "Existe uma estrutura chamada float_vector, que está em algum lugar, e eu estou dando um novo nome pra essa estrutura -> floatVector
